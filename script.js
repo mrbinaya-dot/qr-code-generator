25818e152647955f434cd419c4d29c5fc1c2503f
mrbinay-dot <nrnay166@gmail.com>
2026-02-14 17:37:56 +0545

    completed qr function with javascript

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let qrText = document.getElementById("inputbox");
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImg");
 function getQr(){
- 
+    if(qrText.value.length>0){
     qrImage.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
+    
+    imgBox.classList.add("show-img");
+}else
+{
+    qrText.classList.add('error');
+    setTimeout(()=>{
+        qrText.classList.remove('error');
+    },1000)
+}
 }
